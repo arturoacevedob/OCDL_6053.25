fd401bf19e4fa1cae9e24cd1623548e3996129a2
arturoacevedob <arturoacevedob@gmail.com>
2017-01-18 16:58:11 -0800

    Ex. 12-16

--- ex11.rb
+++ ex11.rb
@@ -4,7 +4,5 @@ print "How tall are you? "
 height = gets.chomp
 print "how much do you weight? "
 weight = gets.chomp
-integer = gets.chomp.to_i
-
 
 puts "So, you're #{age} years old, #{height} tall, and #{weight} heavy."
--- ex12.rb
+++ ex12.rb
@@ -1,4 +1,12 @@
 print "Type a value: "
 number = gets.chomp.to_i
 
-puts "#{number + 1}"
+bigger = number + 1
+puts "A bigger number us #{bigger}"
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number - 1
+puts "A smaller number is #{smaller}"
