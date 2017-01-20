b2882c4517f13ff20974b811d95f9068ec319f85
arturoacevedob <arturoacevedob@gmail.com>
2017-01-19 16:50:09 -0800

    Ex. 15-18
    Spend most of the time coding a new exercise 17.

--- ex15.rb
+++ ex15.rb
@@ -2,12 +2,7 @@ filename = ARGV.first
 
 txt = open(filename)
 
-puts "Here's you file #{filename}"
+puts "\n"
+puts "Here is the file '#{filename}':"
+puts "\n"
 print txt.read
-
-print "Type the filename again: "
-file_again = $stdin.gets.chomp
-
-txt_again = open(file_again)
-
-print txt_again.read
